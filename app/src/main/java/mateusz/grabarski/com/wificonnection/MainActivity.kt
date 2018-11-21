f777348a65471cb107c769fc1200794523c77a3f
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-11-21 08:44:05 +0100

    connects with typed wifi

--- app/src/main/java/mateusz/grabarski/com/wificonnection/MainActivity.kt
+++ app/src/main/java/mateusz/grabarski/com/wificonnection/MainActivity.kt
@@ -1,12 +1,32 @@
 package mateusz.grabarski.com.wificonnection
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.thanosfisherman.wifiutils.WifiUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var wifiManager: WifiManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        WifiUtils.enableLog(true);
+
+        wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        connect.setOnClickListener { connectToWiFi(ssid = ssid.text.toString(), password = password.text.toString()) }
+    }
+
+    private fun connectToWiFi(ssid: String, password: String) {
+        WifiUtils.withContext(applicationContext)
+            .connectWith(ssid, password)
+            .onConnectionResult { Log.d("MainActivityWIFI", "result, $it") }
+            .start();
     }
 }
